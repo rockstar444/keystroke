dea6b3612a270405a0fd06981dd766aa207be8e4
rockstar444 <135048424+rockstar444@users.noreply.github.com>
2025-02-17 00:29:20 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import OTPPage from './pages/OTPPage';
+import DashboardPage from './pages/DashboardPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/otp" element={<OTPPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface KeystrokeData {
+  key: string;
+  press_time: number;
+  release_time: number;
+}
+
+export interface Location {
+  latitude: number;
+  longitude: number;
+}
